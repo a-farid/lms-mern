aaf051c5d0824700f162097f5fe0a413682902d7
a-farid <fahizoune10@gmail.com>
2024-04-22 15:54:30 +0200

    Init app

--- app.ts
+++ app.ts
@@ -1,2 +1,19 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+require("dotenv").config();
 export const app = express();
+
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: "50mb" }));
+
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({ message: "Hello World" });
+});
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: "Not Found" });
+});
